fa2592822df3b1c3317856c5c53894976451492d
James F <james.j.farrugia@gmail.com>
2021-01-21 13:27:43 -0800

    HW2

---
+++ HW2/js/script.js
@@ -0,0 +1,6 @@
+// writes some jazz to the console
+console.log("Fav sites: reddit, pinkbike, youtube");
+console.log("Fav games in particular order: WoW, DoTA 2, Portal");
+console.log("Fav artists: Banksy, Lil Peep, Del tha Funkee Homosapien")
+
+
