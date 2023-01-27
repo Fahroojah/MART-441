a6cdaafeefe127c62b743497778b23ddd198a2f6
James F <70186613+Fahroojah@users.noreply.github.com>
2023-01-27 11:32:00 -0700

    1/27/23
    Minor tweaks

--- HW2/js/script.js
+++ HW2/js/script.js
@@ -1,6 +1,12 @@
-// writes some jazz to the console
+// writes some jazz to the console ; this is a single-line comment
 console.log("Fav sites: reddit, pinkbike, youtube");
-console.log("Fav games in particular order: WoW, DoTA 2, Portal");
+console.log("Fav games in no particular order: WoW, DoTA 2, Portal");
 console.log("Fav artists: Banksy, Lil Peep, Del tha Funkee Homosapien")
-
+/*
+This
+is
+a
+multi-line
+comment
+*/
 
