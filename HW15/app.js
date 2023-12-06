336a9e644f8e53e01d66937daa7f867dc4159602
James F <james.j.farrugia@gmail.com>
2023-12-06 14:58:21 -0600

    HW15

---
+++ HW15/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve your CSS as static
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/submit', (req, res) => {
+    const { name, email } = req.body;
+
+    // Write data to a text file
+    fs.writeFile('userData.txt', `Name: ${name}, Email: ${email}`, (err) => {
+        if (err) throw err;
+    });
+
+    // Redirect to the second page with query string
+    res.redirect(`/success?name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}`);
+});
+
+app.get('/success', (req, res) => {
+    res.sendFile(__dirname + '/success.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
